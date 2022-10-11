b34ce811da6af418520ede98eab3cd5aec621d41
Yongyuth Chuankhuntod <eine.schlacht@gmail.com>
2022-10-11 19:45:59 +0700

    @Yothgewalt: has been initial svelte-app for ine webpage.

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+import WindiCSS from 'vite-plugin-windicss';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/ine-fitm-kmutnb-ac-th",
+    plugins: [
+        svelte(),
+        WindiCSS()
+    ],
+    resolve: {
+        alias: {
+        }
+    }
+});
